fc9949cfeeee21572eb5abc53c7ca80f746fc03f
Bouhlel El Bechri <bouhlel_elbechri@web.de>
2020-02-12 09:46:57 +0100

    Creation de HTML avec JSX

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import './App.css'
 function App () {
   return (
     <div className='container'>
-      
+      <div className='row'>
+        <div className='col-sm-6'>
+          <textarea  className='form-control' rows="35" cols="">
+          
+          </textarea>
+        </div>
+        <div className='col-sm-6'>
+          <h1>
+            Résultat
+          </h1>
+        </div>
+      </div>
     </div>
   )
 }
